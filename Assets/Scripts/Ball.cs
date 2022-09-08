16120b3431ae6a2052c0ef821e3f13911479ed04
Danil0990 <wpigyn@gmail.com>
2022-09-08 19:00:00 +0500

    55

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,11 +10,12 @@ public class Ball : MonoBehaviour
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        _rigidbody.AddForce(Vector3.back * 100f);
+        _rigidbody.AddForce(Vector3.back * 500f);
     }
 
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
         
+        _rigidbody.AddForce(Vector3.back * 500f);
     }
 }
